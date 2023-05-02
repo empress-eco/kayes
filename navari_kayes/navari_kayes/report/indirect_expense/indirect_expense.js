9d39e6848cf6cd5636aabba7a8425388b5615a6d
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-05-02 13:41:36 +0300

    feat: init indirect expense report

---
+++ navari_kayes/navari_kayes/report/indirect_expense/indirect_expense.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, Navari Limited and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Indirect Expense"] = {
+	"filters": [
+
+	]
+};
