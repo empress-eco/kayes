977bbb671cac85a2be85ced01ffa96a2bf3f116f
Lucky-Tsuma <tsumahmumba@gmail.com>
2023-05-02 14:19:00 +0300

    feat: add report filters

--- navari_kayes/navari_kayes/report/indirect_expense/indirect_expense.js
+++ navari_kayes/navari_kayes/report/indirect_expense/indirect_expense.js
@@ -4,6 +4,60 @@
 
 frappe.query_reports["Indirect Expense"] = {
 	"filters": [
-
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"width": "100px",
+			"reqd": 1
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"reqd": 1,
+			"width": "100px"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date"),
+			"reqd": 1,
+			"width": "100px"
+		},
+		{
+			"fieldname":"id",
+			"label": __("Id"),
+			"fieldtype": "Link",
+			"options": "GL Entry",
+			"reqd": 0,
+			"width": "100px"
+		},
+		{
+			"fieldname":"account",
+			"label": __("Account"),
+			"fieldtype": "Link",
+			"options": "Account",
+			"reqd": 0,
+			"width": "100px"
+		},
+		{
+			"fieldname":"party",
+			"label": __("Party"),
+			"fieldtype": "Data",
+			"reqd": 0,
+			"width": "100px"
+		},
+		{
+			"fieldname":"voucher_no",
+			"label": __("Voucher No"),
+			"fieldtype": "Data",
+			"reqd": 0,
+			"width": "100px"
+		}
 	]
 };
